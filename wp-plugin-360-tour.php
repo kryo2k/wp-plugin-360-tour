42071f8ecc75c5ae06802eefc433225229213121
Hans Doller <kryo2k@gmail.com>
2013-11-10 03:41:10 -0600

    initial files

---
+++ wp-plugin-360-tour.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Plugin Name: 360 tour plugin
+ * Description: Custom plugin to add support for 360 tour service (provided by http://360.cr/) implemented on hotelarenalkioro.com
+ * Version: 0.1
+ * Author: Hans Doller
+ * Author URI: http://ticonerd.com
+ * License: GPLv3
+ */
+
+
