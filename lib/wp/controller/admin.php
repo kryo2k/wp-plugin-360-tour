62db54321bc322b90dc709d861bc6d07b354ecbe
Hans Doller <kryo2k@gmail.com>
2013-11-10 05:12:26 -0600

    baby steps..

--- lib/wp/controller/admin.php
+++ lib/wp/controller/admin.php
@@ -2,3 +2,21 @@
 
 function t360_init() {
 }
+
+add_action( 'admin_menu', 't360_admin_menu', 9 );
+
+function t360_admin_menu() {
+	add_object_page( __( '360 Tour Manager', 't360' ), __( '360 Tour', 't360' ),
+	't360_read_contact_forms', 't360', 't360_admin_management_page',
+	t360_path_resource( 'image/admin/menu-icon.png' ) );
+
+/*	$contact_form_admin = add_submenu_page( 't360',
+			__( 'Edit Contact Forms', 't360' ), __( 'Edit', 't360' ),
+			't360_read_contact_forms', 't360', 't360_admin_management_page' ); */
+
+//	add_action( 'load-' . $contact_form_admin, 't360_load_contact_form_admin' );
+}
+
+function t360_admin_management_page() {
+	echo "Hello World!";
+}
