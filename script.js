f11cc2429faa8cdad861dce8408f2acdd69d262d
Hans Doller <kryo2k@gmail.com>
2013-11-10 19:36:09 -0600

    More script changes

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 (function($){
 	if($ === undefined) throw 'jQuery is not installed. This is required.';
-	var cfg = window.t360_config;
 
-	console.log("t360 loaded. Config:", cfg);
+	function T360(cfg){
+		return this;
+	};
+
+	$(function(){
+		var
+		config = window.t360_config,
+		instance = new T360(config);
+		console.log("t360 loaded. config:", config, ', instance:', instance);
+	});
+
 })(jQuery);
